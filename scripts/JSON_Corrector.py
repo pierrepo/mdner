2bd96a9a0d16a8846762842037a10b5d813f2525
Mohamed Oussaren <oussarenmohamed@outlook.fr>
2023-03-08 16:46:15 +0100

    Add save_json function and others

--- scripts/JSON_Corrector.py
+++ scripts/JSON_Corrector.py
@@ -11,7 +11,7 @@ def display_ner(data_json, json_edit):
         "TEMPERATURE": "#FF0000",
         "SOFTWARE": "#FFA500",
         "SIMULATION TIME": "#FD6C9E",
-        "MODEL": "#00FFFF",
+        "FF & MODEL": "#00FFFF",
         "MOLECULE": "#FFFF00",
     }
     options = {
@@ -27,8 +27,8 @@ def display_ner(data_json, json_edit):
     st.markdown(ent_html, unsafe_allow_html=True)
 
 
-def display_editor(data_json):
-    col_editor, col_display = st.columns([1, 1])
+def display_editor(name_file, data_json):
+    col_editor, col_display = st.columns([1, 2])
     with col_editor:
         ent_json = json.dumps(data_json["annotations"][0][1], indent=4)
         new_json = st.text_area(
@@ -36,37 +36,21 @@ def display_editor(data_json):
             ent_json,
             height=600,
         )
-        with col_display:
-            try:
-                display_ner(data_json, new_json)
-            except Exception:
-                st.error("This json is not well written !", icon="🚨")
-                return ent_json, False
+    with col_display:
+        try:
+            st.markdown(
+                f"<p class='font-label'>JSON preview : {name_file}</p>",
+                unsafe_allow_html=True,
+            )
+            display_ner(data_json, new_json)
+        except Exception:
+            st.error("This json is not well written !", icon="🚨")
+            return ent_json, False
     return new_json, new_json != ent_json
 
 
-def load_css():
-    st.markdown(
-        """
-                <style> 
-                    .block-container:first-of-type {
-                        padding-top: 20px;
-                        padding-left: 20px;
-                        padding-right: 20px;
-                    }
-                </style>
-                """,
-        unsafe_allow_html=True,
-    )
-
-
-def user_interaction():
-    st.set_page_config(page_title="JSON Corrector", layout="wide")
-    load_css()
-    st.title("JSON Corrector")
-    path = "../annotations/"
-    files = [file.split("/")[-1].split(".")[0] for file in glob.glob(path + "*.json")]
-    if files:
+def display_filters(files):
+    if len(files) > 1:
         st.sidebar.title("Filters")
         select_json = st.sidebar.slider(
             "Choose a JSON file to correct:", 1, len(files), 1
@@ -78,19 +62,54 @@ def user_interaction():
             path_name = "../annotations/" + search_json
         else:
             path_name = "../annotations/" + files[select_json - 1] + ".json"
+    else:
+        path_name = "../annotations/" + files[0] + ".json"
+    return path_name
+
+
+def load_css():
+    st.markdown(
+        """
+                <style>
+                    .block-container:first-of-type {
+                        padding-top: 20px;
+                        padding-left: 20px;
+                        padding-right: 20px;
+                    }
+                    .font-label {
+                        font-size: 14px !important;
+                    }
+                </style>
+                """,
+        unsafe_allow_html=True,
+    )
+
+
+def save_json(data_json, new_json, path_name, edited):
+    save = st.button("Save", disabled=not edited)
+    if save:
+        f = open(path_name, "w")
+        ent_save = re.sub("\n| ", "", new_json)
+        data_json["annotations"][0][1] = json.loads(ent_save)
+        save_json = json.dumps(data_json, indent=None)
+        f.write(save_json)
+        f.close()
+        st.success("JSON file saved !", icon="✅")
+
+
+def user_interaction():
+    st.set_page_config(page_title="JSON Corrector", layout="wide")
+    load_css()
+    st.title("JSON Corrector")
+    path = "../annotations/"
+    files = [file.split("/")[-1].split(".")[0] for file in glob.glob(path + "*.json")]
+    if files:
+        path_name = display_filters(files)
         f = open(path_name, "r")
         data_json = json.load(f)
         f.close()
-        new_json, edited = display_editor(data_json)
-        save = st.button("Save", disabled=not edited)
-        if save:
-            f = open(path_name, "w")
-            ent_save = re.sub("\n| ", "", new_json)
-            data_json["annotations"][0][1] = json.loads(ent_save)
-            save_json = json.dumps(data_json, indent=None)
-            f.write(save_json)
-            f.close()
-            st.success("JSON file saved !", icon="✅")
+        new_json, edited = display_editor(path_name.split("/")[-1], data_json)
+        save_json(data_json, new_json, path_name, edited)
     else:
         st.error("No JSON file found !", icon="🚨")
 
