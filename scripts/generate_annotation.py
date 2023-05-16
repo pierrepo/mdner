f17d1174e323c0f105ad080389078515b8dd166e
Mohamed Oussaren <oussarenmohamed@outlook.fr>
2023-05-16 17:24:57 +0200

    Add paraphrase functions

--- scripts/generate_annotation.py
+++ scripts/generate_annotation.py
@@ -8,10 +8,11 @@ import argparse
 import glob
 import os
 import json
-from datetime import datetime
 import unicodedata
 import re
 import logging
+from transformers import BartForConditionalGeneration, BartTokenizer
+import spacy
 
 parser = argparse.ArgumentParser(
     description="Generate text and json files in the annotation folder to be used as training sets."
@@ -28,6 +29,9 @@ parser.add_argument(
     help="Select the descriptive texts where the cosine similarity is below the threshold.",
     nargs="?",
     default=0.2,
+)
+parser.add_argument(
+    "-d", "--duplicate", help="Duplicate the annotation.", action="store_true"
 )
 args = parser.parse_args()
 
@@ -140,6 +144,33 @@ def clear_annotation(annotation: str):
     return annotation
 
 
+def create_json(path_file: str, text: str, ents: list):
+    """
+    Create a json file for each annotation.
+    
+    Parameters
+    ----------
+    path_file: str
+        The path of the file.
+    text: str
+        The text of the annotation.
+    ents: list
+        The entities of the annotation.
+    """
+    with open(path_file, "w") as json_file:
+        dict_annotations = {
+            "classes": [
+                "TEMP",
+                "SOFT",
+                "STIME",
+                "MOL",
+                "FFM",
+            ],
+            "annotations": [[text, {"entities": ents}]],
+        }
+        json.dump(dict_annotations, json_file)
+
+
 def create_annotation(df: pd.DataFrame):
     """
     Create a file for each annotation.
@@ -163,21 +194,7 @@ def create_annotation(df: pd.DataFrame):
         ) as f:
             f.write(df.loc[i, "text_dataset"])
         if not os.path.exists(path_file + ".json"):
-            with open(
-                path_file + ".json",
-                "w",
-            ) as json_file:
-                dict_annotations = {
-                    "classes": [
-                        "TEMP",
-                        "SOFT",
-                        "STIME",
-                        "MOL",
-                        "FFM",
-                    ],
-                    "annotations": [[df.loc[i, "text_dataset"], {"entities": []}]],
-                }
-                json.dump(dict_annotations, json_file)
+            create_json(path_file + ".json", df.loc[i, "text_dataset"], [])
 
 
 def generate_annotation(threshold: int, cutoff: float):
@@ -216,6 +233,119 @@ def clear_folder():
     logging.info("Folder cleared")
 
 
+# ------------------------------ START IN PROGRESS -----------------------------------#
+
+
+def load_model_paraphrase():
+    model = BartForConditionalGeneration.from_pretrained("eugenesiow/bart-paraphrase")
+    tokenizer = BartTokenizer.from_pretrained("eugenesiow/bart-paraphrase")
+    return model, tokenizer
+
+
+def get_entities(annotations, text):
+    ents = []
+    for ent in annotations["entities"]:
+        ents.append((text[ent[0] : ent[1]], ent[2]))
+    return ents
+
+
+def generate_paraphrase(input_sentence, model, tokenizer):
+    batch = tokenizer(input_sentence, return_tensors="pt", padding="longest")
+    generated_ids = model.generate(
+        batch["input_ids"],
+        temperature=1.2,
+        num_beams=8,
+        num_beam_groups=8,
+        early_stopping=True,
+        num_return_sequences=1,
+        repetition_penalty=10.0,
+        diversity_penalty=3.0,
+        max_length=1024,
+    )
+    generated_sentence = tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+    return generated_sentence
+
+def split_text(text):
+    # List of punctuation to identify sentences
+    punctuations = ["."]
+    
+    # Divide the text into parts with a maximum size of 1024 characters
+    parts = []
+    while len(text) > 0:
+        # Find the last punctuation in the part
+        last_punctuation_index = -1
+        for punctuation in punctuations:
+            index = text.rfind(punctuation, 0, 1024)
+            if index > last_punctuation_index:
+                last_punctuation_index = index
+        
+        if last_punctuation_index == -1:
+            # No punctuation found, divide the part to 1024 characters
+            parts.append(text[:1024])
+            text = text[1024:]
+        else:
+            # Divide the part at the last punctuation found
+            parts.append(text[:last_punctuation_index+1])
+            text = text[last_punctuation_index+1:]
+    
+    return parts
+
+def predict_paraphrase(text, model, tokenizer) :
+    # The length must be less than 1024 characters according the model
+    if len(text) < 1024:
+        predict_text = generate_paraphrase(text, model, tokenizer)[0]
+    else :
+        # Split the text into parts with a maximum size of 1024 characters
+        splited_text = split_text(text)
+        predict_text = " ".join(generate_paraphrase(text)[0] for text in splited_text)
+    return predict_text
+
+def found_entity(to_found, text, entities):
+    # Find the entity in the text
+    exist = False
+    to_found = to_found.lower()
+    text = text.lower()
+    nlp = spacy.blank("en")
+    doc = nlp.make_doc(text)
+    for i in range(len(text)):
+        if text.startswith(to_found, i):
+            start = i
+            end = i + len(to_found)
+            # Check if the found entity can be aligned
+            span = doc.char_span(start, end, label="TEST", alignment_mode="strict")
+            if span is not None:
+                # Check if the entity already exists
+                for ent in entities:
+                    if (ent[0] == start or ent[1] == end) or (
+                        start > ent[0] and end < ent[1]
+                    ):
+                        exist = True
+                if not exist:
+                    return start, end
+            exist = False
+    return None
+
+
+def duplicate_annotation():
+    path = "annotations/"
+    model, tokenizer = load_model_paraphrase()
+    for json_file in glob.glob(path + "*.json"):
+        path_duplicate = (
+            path + json_file.split(".")[2].split("/")[2] + "_" + "duplicate" + ".json"
+        )
+        if "duplicate" not in json_file or not os.path.exists(path_duplicate):
+            with open(json_file, "r") as f_json:
+                c_json = json.load(f_json)
+                text = c_json["annotations"][0][0]
+                annotations = c_json["annotations"][0][1]
+                ents = get_entities(annotations)
+                predict_text = predict_paraphrase(text, model, tokenizer)
+                new_entities = [[pos[0], pos[1], ent[1]] for ent in ents if (pos := found_entity(ent[0], predict_text, new_entities)) is not None]
+                create_json(path_duplicate, predict_text, new_entities)
+
+
+# ------------------------------ END IN PROGRESS -----------------------------------#
+
 if __name__ == "__main__":
     logging.basicConfig(
         format="[%(asctime)s] [%(levelname)s] %(message)s",
@@ -224,3 +354,5 @@ if __name__ == "__main__":
     if args.clear:
         clear_folder()
     generate_annotation(int(args.threshold), float(args.cutoff))
+    if args.duplicate:
+        duplicate_annotation()
