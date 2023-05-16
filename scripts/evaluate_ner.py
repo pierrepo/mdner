96194f6553eddb5aaf1fb26dbca443966ca20e87
Mohamed Oussaren <oussarenmohamed@outlook.fr>
2023-05-16 16:35:49 +0200

    Change outputs for some cells

--- scripts/evaluate_ner.py
+++ scripts/evaluate_ner.py
@@ -3,13 +3,14 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 import spacy
 
+
 def get_scores(paths: list) -> list:
     # Define metrics to plot
     metrics = ["ENTS_F", "ENTS_P", "ENTS_R"]
-    
+
     # Define lists to store scores, loss values and epochs
     results = []
-    
+
     # Loop over paths to extract metrics and loss values
     for path in paths:
         with open(path, "r") as f:
@@ -23,10 +24,13 @@ def get_scores(paths: list) -> list:
                     scores["ENTS_P"].append(float(line.split()[-3]))
                     scores["ENTS_R"].append(float(line.split()[-2]))
                     loss_validation.append(float(line.split()[3]))
-            results.append({"epoch": epoch, "scores": scores, "loss_validation": loss_validation})
+            results.append(
+                {"epoch": epoch, "scores": scores, "loss_validation": loss_validation}
+            )
     return results
 
-def display_plots(results: list, names_model: list):            
+
+def display_plots(results: list, names_model: list):
     # Create plots for metrics and loss evolution
     _, axs = plt.subplots(len(names_model), 2, figsize=(12, 10))
 
@@ -34,29 +38,38 @@ def display_plots(results: list, names_model: list):
         # Create a plot for metrics evolution
         df_scores = pd.DataFrame.from_dict(result["scores"])
         df_scores["epoch"] = result["epoch"]
-        sns.lineplot(x="epoch", y="value", hue="variable", data=pd.melt(df_scores, ["epoch"]), ax=axs[0][i])
+        sns.lineplot(
+            x="epoch",
+            y="value",
+            hue="variable",
+            data=pd.melt(df_scores, ["epoch"]),
+            ax=axs[0][i],
+        )
         axs[0][i].set_title("Metrics evolution of " + names_model[i])
         axs[0][i].set_xlabel("Epoch")
         axs[0][i].set_ylabel("Scores (%)")
         axs[0][i].set_ylim(top=100)
 
         # Create a plot for loss evolution
-        df_loss = pd.DataFrame({"epoch": result["epoch"], "loss_validation": result["loss_validation"]})
+        df_loss = pd.DataFrame(
+            {"epoch": result["epoch"], "loss_validation": result["loss_validation"]}
+        )
         sns.lineplot(x="epoch", y="loss_validation", data=df_loss, ax=axs[1][i])
         axs[1][i].set_title("Loss evolution of " + names_model[i])
         axs[1][i].set_xlabel("Epoch")
         axs[1][i].set_ylabel("Loss value")
-        
+
+
 def get_entities(doc, only_mol):
     ents = [(ent.text, ent.start_char, ent.end_char, ent.label_) for ent in doc.ents]
     if only_mol:
         ents = [ent for ent in ents if ent[3] == "MOL"]
     return ents
 
-def get_confusion_matrix(name_model, only_mol):
-    path = f"../results/colab/{name_model}"
-    ner = spacy.load(f"{path}/models/model-best/")
-    with open(f"../results/colab/{name_model}/eval_data.spacy", "rb") as f:
+
+def get_confusion_matrix(path_model, only_mol):
+    ner = spacy.load(f"{path_model}/model-best/")
+    with open(f"{path_model}/eval_data.spacy", "rb") as f:
         doc_bin = spacy.tokens.DocBin().from_bytes(f.read())
     tp, fp, fn, tn = 0, 0, 0, 0
     for doc in doc_bin.get_docs(ner.vocab):
@@ -69,19 +82,37 @@ def get_confusion_matrix(name_model, only_mol):
     confusion_matrix = pd.DataFrame(
         [[tp, fp], [fn, tn]],
         index=["Positive", "Negative"],
-        columns=["Positive", "Negative"]
+        columns=["Positive", "Negative"],
     )
+    acc = (tp + tn) / (tp + fp + fn + tn) * 100
     p = tp / (tp + fp) * 100
     r = tp / (tp + fn) * 100
-    return confusion_matrix, [p, r]
+    return confusion_matrix, [acc, p, r]
 
-def display_confusion_matrix(cf_mtx, names_model: list):
-    # Display confusion matrix    
-    _, axs = plt.subplots(1, len(names_model), figsize=(12, 3))
-    for i, name in enumerate(names_model):
-        sns.heatmap(cf_mtx[name], annot=True, cmap='Blues', fmt='d', ax=axs[i])
-        axs[i].set_xlabel('Annotated')
-        axs[i].set_ylabel('Predict')
-        axs[i].set_title("Confusion matrix of " +  name, y=-0.25)
-        axs[i].tick_params(axis='both', which='major', labelsize=10, labelbottom = False, bottom=False, top = False, labeltop=True)
-        axs[i].xaxis.set_label_position('top') 
+
+def display_confusion_matrix(cf_mtx, scores, paths_model: list):
+    # Display confusion matrix
+    _, axs = plt.subplots(1, len(paths_model), figsize=(12, 3))
+    for i, path in enumerate(paths_model):
+        sns.heatmap(cf_mtx[path], annot=True, cmap="Blues", fmt="d", ax=axs[i])
+        axs[i].set_xlabel("Annotated")
+        axs[i].set_ylabel("Predict")
+        axs[i].set_title("Confusion matrix of " + path.split("/")[-1], y=-0.25)
+        axs[i].tick_params(
+            axis="both",
+            which="major",
+            labelsize=10,
+            labelbottom=False,
+            bottom=False,
+            top=False,
+            labeltop=True,
+        )
+        axs[i].xaxis.set_label_position("top")
+        print_score = f"Accuracy: {round(scores[path][0], 2)}\nPrecision: {round(scores[path][1], 2)}\nRecall: {round(scores[path][2], 2)}"
+        axs[i].text(
+            0.75,
+            3.1,
+            print_score,
+            style="italic",
+            bbox={"facecolor": "blue", "alpha": 0.5, "pad": 10},
+        )
