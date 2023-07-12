1b457ec6caf9ec8a8aeef08ab319f80096d77d7a
Moohmoo <oussarenmohamed@gmail.com>
2023-07-12 12:57:08 +0200

    Update comments

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,14 +13,14 @@ done
 
 for model_name in "${model_names[@]}"
 do
-    # Define the parameters for each model
+    # Define the parameters for paraphrase and basic models
     params1="-c -t 0.1 0.0 1.0 0.0 -n ${model_name}_paraphrase -g -p -s 7522"
     params2="-c -t 0.1 0.0 1.0 0.0 -n ${model_name} -g -s 7522"
 
-    # Print the model name and parameters
+    # Print the model name
     echo "Building model $model_name"
 
-    # Call the Python scripts with the parameters
+    # Call the Python scripts with the parameters for paraphrase and basic models
     python scripts/mdner.py $params1
     python scripts/mdner.py $params2
 done
